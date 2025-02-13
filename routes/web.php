e13009ffb33d42590fbd657bcc10cd83d43b923b
Diego Pereira Betti <diegopbetti.gmail.com>
2025-02-12 21:20:09 -0300

    Adiciona a home page e cria sua rota

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomePageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -16,5 +17,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/home_page', HomePageController::class)->name('home_page');
 
 require __DIR__.'/auth.php';
