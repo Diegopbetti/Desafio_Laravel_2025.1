75ae8b6384803c6a0a700c29b6197bfaf81994f2
Diego Pereira Betti <diegopbetti.gmail.com>
2025-02-16 17:09:24 -0300

    Adiciona aparencia do botao para filtrar as pesquisas e paginacao

--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -9,9 +9,12 @@
 <body class="flex flex-col items-center">
     <div class="flex justify-between items-center h-20 bg-blue-950 text-white w-full">
         <h1 class="text-3xl ml-10">TechMarket</h1>
-        <input class="bg-blue-950 rounded-xl h-8 mr-10 " placeholder="Pesquisar"></input>
+        <div>
+            <button class="underline mr-2 transition-transform duration-300 hover:scale-110">Filtrar</button>
+            <input class="bg-blue-950 rounded-xl h-8 mr-10 " placeholder="Pesquisar"></input>
+        </div>
     </div>
-    <di class="grid grid-cols-3 gap-x-5 gap-y-5 w-1/2 mt-12">
+    <div class="grid grid-cols-3 gap-x-5 gap-y-5 w-1/2 mt-12">
         <div class="flex flex-col items-center bg-blue-950 w-48 rounded-xl p-3">
             <img src="{{ asset('images/celular.webp') }}" alt="" class="w-48 h-40 rounded-xl object-contain">     
             <div class="flex flex-col w-full text-white mt-3">
@@ -129,5 +132,11 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="flex justify-center m-8 w-full text-white">
+        <button class="bg-blue-950 mx-1 px-3 py-1 rounded-xl">1</button>
+        <button class="bg-blue-500 mx-1 px-3 py-1 rounded-xl">2</button>
+        <button class="bg-blue-500 mx-1 px-3 py-1 rounded-xl">3</button>
+        <button class="bg-blue-500 mx-1 px-3 py-1 rounded-xl">4</button>
     </div>
 </body>
