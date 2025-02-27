805961a4d130a0228b6760a19a3e06d6ae87e557
Diego Pereira Betti <diegopbetti.gmail.com>
2025-02-27 13:33:39 -0300

    Modifica o que aparece no gerenciamento de usuario e passa os usuarios do banco de dados

--- app/Http/Controllers/UserManagementController.php
+++ app/Http/Controllers/UserManagementController.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
 class UserManagementController extends Controller
 {
     public function __invoke(){
-        return view('user_management');
+        $users = User::paginate(5);
+
+        return view('user_management', compact('users'));
     }
 }
--- resources/views/user_management.blade.php
+++ resources/views/user_management.blade.php
@@ -23,15 +23,9 @@
             <table class="w-full overflow-hidden text-white rounded-[10px] border-collapse">
                 <thead class="bg-blue-950">
                     <tr class="bg-[#237ecd] text-white">
+                        <th class="px-3 py-2 text-center">Id</th>
                         <th class="px-3 py-2 text-center">Name</th>
                         <th class="px-3 py-2 text-center">E-mail</th>
-                        <th class="px-3 py-2 text-center">Password</th>
-                        <th class="px-3 py-2 text-center">Address</th>
-                        <th class="px-3 py-2 text-center">Telephone</th>
-                        <th class="px-3 py-2 text-center">Birth</th>
-                        <th class="px-3 py-2 text-center">CPF</th>
-                        <th class="px-3 py-2 text-center">Balance</th>
-                        <th class="px-3 py-2 text-center">Photo</th>
                         <th class="px-3 py-2 text-center">View</th>
                         <th class="px-3 py-2 text-center">Edit</th>
                         <th class="px-3 py-2 text-center">Delete</th>
@@ -39,24 +33,24 @@
                     </tr>
                 </thead>
                 <tbody class="bg-blue-950">
+                    @foreach($users as $user)
                     <tr class="border-b border-gray-600">
-                        <td class="px-3 py-2 text-center">Vitão</td>
-                        <td class="px-3 py-2 text-center">vitao@vitao.com</td>
-                        <td class="px-3 py-2 text-center">fefefwefw</td>
-                        <td class="px-3 py-2 text-center">Uganda</td>
-                        <td class="px-3 py-2 text-center">4002-8922</td>
-                        <td class="px-3 py-2 text-center">12/12/2012</td>
-                        <td class="px-3 py-2 text-center">12345678900</td>
-                        <td class="px-3 py-2 text-center">R$-22,00</td>
-                        <td class="px-3 py-2 text-center">-</td>
+                        <td class="px-3 py-2 text-center">{{$user->id}}</td>
+                        <td class="px-3 py-2 text-center">{{$user->name}}</td>
+                        <td class="px-3 py-2 text-center">{{$user->email}}</td>
+                        <!-- <td class="px-3 py-2 text-center">********</td>
+                        <td class="px-3 py-2 text-center">{{$user->address}}</td>
+                        <td class="px-3 py-2 text-center">{{$user->telephone}}</td>
+                        <td class="px-3 py-2 text-center">{{$user->birth_date}}</td>
+                        <td class="px-3 py-2 text-center">{{$user->cpf}}</td>
+                        <td class="px-3 py-2 text-center">R${{$user->balance}}</td> -->
                         <th class="px-3 py-2 text-center"><button class="btn-acao bg-[#00AEA0] inline-flex items-center justify-center w-[20px] h-[20px] rounded-md border-none mt-1 cursor-pointer"></button></th>
                         <th class="px-3 py-2 text-center"><button class="btn-acao bg-[#FFC739] inline-flex items-center justify-center w-[20px] h-[20px] rounded-md border-none mt-1 cursor-pointer"></button></th>
                         <th class="px-3 py-2 text-center"><button class="btn-acao bg-[#C70E3C] inline-flex items-center justify-center w-[20px] h-[20px] rounded-md border-none mt-1 cursor-pointer"></button></th>
-
                     </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
     </main>
 </body>
-</html>
