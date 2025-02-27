e1ad5f77992cc1005674324d36e213338d2e77de
Diego Pereira Betti <diegopbetti.gmail.com>
2025-02-27 00:02:30 -0300

    Modifica a home_page para aparecer as informacoes de cada produto

--- app/Http/Controllers/HomePageController.php
+++ app/Http/Controllers/HomePageController.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Product;
 
 use Illuminate\Http\Request;
 
 class HomePageController extends Controller
 {
     public function __invoke(){
-        return view('home_page');
+        $products = Product::paginate(9);
+
+        return view('home_page', compact('products'));
     }
 }
--- resources/views/home_page.blade.php
+++ resources/views/home_page.blade.php
@@ -15,123 +15,21 @@
         </div>
     </div>
     <div class="grid grid-cols-3 gap-x-5 gap-y-5 w-1/2 mt-12">
+        @foreach($products as $product)
         <div class="flex flex-col items-center bg-blue-950 w-48 rounded-xl p-3">
-            <img src="{{ asset('images/celular.webp') }}" alt="" class="w-48 h-40 rounded-xl object-contain">     
+            <img src="{{ asset($product->photo) }}" alt="{{$product->name}}" class="w-48 h-40 rounded-xl object-contain">     
             <div class="flex flex-col w-full text-white mt-3">
                 <div class="flex justify-between items-center w-full">
-                    <span class="ml-1 font-bold">Celular</span>
-                    <span class="mr-1 text-right">$1500,00</span>  
+                    <span class="ml-1 font-bold">{{ $product->name }}</span>
+                    <span class="mr-1 text-right">${{ $product->price }}</span>  
                 </div>
                 <div class="flex justify-between w-full mt-4">
-                    <a href="{{ route('individual_page') }}" class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Visualizar</a>                         
+                    <a href="{{ route('individual_page', $product->id) }}}" class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Visualizar</a>                         
                     <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Comprar</button>
                 </div>
             </div>
         </div>
-        <div class="flex flex-col items-center bg-blue-950 w-48 rounded-xl p-3">
-            <img src="{{ asset('images/celular.webp') }}" alt="" class="w-48 h-40 rounded-xl object-contain">     
-            <div class="flex flex-col w-full text-white mt-3">
-                <div class="flex justify-between items-center w-full">
-                    <span class="ml-1 font-bold">Celular</span>
-                    <span class="mr-1 text-right">$1500,00</span>  
-                </div>
-                <div class="flex justify-between w-full mt-4">
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Visualizar</button>                         
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Comprar</button>
-                </div>
-            </div>
-        </div>
-        <div class="flex flex-col items-center bg-blue-950 w-48 rounded-xl p-3">
-            <img src="{{ asset('images/celular.webp') }}" alt="" class="w-48 h-40 rounded-xl object-contain">     
-            <div class="flex flex-col w-full text-white mt-3">
-                <div class="flex justify-between items-center w-full">
-                    <span class="ml-1 font-bold">Celular</span>
-                    <span class="mr-1 text-right">$1500,00</span>  
-                </div>
-                <div class="flex justify-between w-full mt-4">
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Visualizar</button>                         
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Comprar</button>
-                </div>
-            </div>
-        </div>
-        <div class="flex flex-col items-center bg-blue-950 w-48 rounded-xl p-3">
-            <img src="{{ asset('images/celular.webp') }}" alt="" class="w-48 h-40 rounded-xl object-contain">     
-            <div class="flex flex-col w-full text-white mt-3">
-                <div class="flex justify-between items-center w-full">
-                    <span class="ml-1 font-bold">Celular</span>
-                    <span class="mr-1 text-right">$1500,00</span>  
-                </div>
-                <div class="flex justify-between w-full mt-4">
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Visualizar</button>                         
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Comprar</button>
-                </div>
-            </div>
-        </div>
-        <div class="flex flex-col items-center bg-blue-950 w-48 rounded-xl p-3">
-            <img src="{{ asset('images/celular.webp') }}" alt="" class="w-48 h-40 rounded-xl object-contain">     
-            <div class="flex flex-col w-full text-white mt-3">
-                <div class="flex justify-between items-center w-full">
-                    <span class="ml-1 font-bold">Celular</span>
-                    <span class="mr-1 text-right">$1500,00</span>  
-                </div>
-                <div class="flex justify-between w-full mt-4">
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Visualizar</button>                         
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Comprar</button>
-                </div>
-            </div>
-        </div>
-        <div class="flex flex-col items-center bg-blue-950 w-48 rounded-xl p-3">
-            <img src="{{ asset('images/celular.webp') }}" alt="" class="w-48 h-40 rounded-xl object-contain">     
-            <div class="flex flex-col w-full text-white mt-3">
-                <div class="flex justify-between items-center w-full">
-                    <span class="ml-1 font-bold">Celular</span>
-                    <span class="mr-1 text-right">$1500,00</span>  
-                </div>
-                <div class="flex justify-between w-full mt-4">
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Visualizar</button>                         
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Comprar</button>
-                </div>
-            </div>
-        </div>
-        <div class="flex flex-col items-center bg-blue-950 w-48 rounded-xl p-3">
-            <img src="{{ asset('images/celular.webp') }}" alt="" class="w-48 h-40 rounded-xl object-contain">     
-            <div class="flex flex-col w-full text-white mt-3">
-                <div class="flex justify-between items-center w-full">
-                    <span class="ml-1 font-bold">Celular</span>
-                    <span class="mr-1 text-right">$1500,00</span>  
-                </div>
-                <div class="flex justify-between w-full mt-4">
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Visualizar</button>                         
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Comprar</button>
-                </div>
-            </div>
-        </div>
-        <div class="flex flex-col items-center bg-blue-950 w-48 rounded-xl p-3">
-            <img src="{{ asset('images/celular.webp') }}" alt="" class="w-48 h-40 rounded-xl object-contain">     
-            <div class="flex flex-col w-full text-white mt-3">
-                <div class="flex justify-between items-center w-full">
-                    <span class="ml-1 font-bold">Celular</span>
-                    <span class="mr-1 text-right">$1500,00</span>  
-                </div>
-                <div class="flex justify-between w-full mt-4">
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Visualizar</button>                         
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Comprar</button>
-                </div>
-            </div>
-        </div>
-        <div class="flex flex-col items-center bg-blue-950 w-48 rounded-xl p-3">
-            <img src="{{ asset('images/celular.webp') }}" alt="" class="w-48 h-40 rounded-xl object-contain">     
-            <div class="flex flex-col w-full text-white mt-3">
-                <div class="flex justify-between items-center w-full">
-                    <span class="ml-1 font-bold">Celular</span>
-                    <span class="mr-1 text-right">$1500,00</span>  
-                </div>
-                <div class="flex justify-between w-full mt-4">
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Visualizar</button>                         
-                    <button class="bg-blue-500 w-20 py-1 rounded-full text-xs font-bold text-center transition-transform duration-300 hover:scale-110">Comprar</button>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
     <div class="flex justify-center m-8 w-full text-white">
         <button class="bg-blue-950 mx-1 px-3 py-1 rounded-xl">1</button>
