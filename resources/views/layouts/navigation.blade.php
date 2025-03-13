816bdb605ee23db650add9067eb88b6ee6c75b07
Diego Pereira Betti <diegopbetti.gmail.com>
2025-03-13 09:43:20 -0300

    Adiciona os links para as outras paginas na dashboard

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,18 +2,29 @@
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
-            <div class="flex">
-                <!-- Logo -->
-                <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
-                    </a>
-                </div>
-
+            <div class="flex">                
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex items-center">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('home_page')" :active="request()->routeIs('home_page')">
+                        {{ __('Home Page') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('withdraw')" :active="request()->routeIs('withdraw')">
+                        {{ __('Saque') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('user_management')" :active="request()->routeIs('user_management')">
+                        {{ __('User Management') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('product_management')" :active="request()->routeIs('product_management')">
+                        {{ __('Product Management') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('purchase_history')" :active="request()->routeIs('purchase_history')">
+                        {{ __('Purchase History') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('sales_history')" :active="request()->routeIs('sales_history')">
+                        {{ __('Sales History') }}
                     </x-nav-link>
                 </div>
             </div>
